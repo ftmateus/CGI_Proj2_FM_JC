4e220df8b2e9b62ed29cf4fae7d277f9c48cbe9f
thesmartassole <32884516+thesmartassole@users.noreply.github.com>
2019-11-04 12:27:12 +0000

    zoom e canvas adaptativo funcionam bem

--- TP2_53270_54440_P4.js
+++ TP2_53270_54440_P4.js
@@ -5,8 +5,10 @@ var tx = 0; var ty = 0; var tz = 0;
 var rx = 0; var ry = 0; var rz = 0;
 var sx = 1; var sy = 1; var sz = 1;
 var zoom = 1;
-var mView; var mProjection; var mModel; var mModelLocation;
+var mView, mViewLocation; var mProjection, mProjectionLocation; var mModel, mModelLocation;
 var isFilled = false, cullFace = false, zBuffer = false;
+const HEIGHT_RATIO = 0.6;
+var zoom = 1;
 //const CTM = mult(mat4(), scalem(1,canvas.width/canvas.height, 1));
 
 window.onload = function init() {
@@ -15,26 +17,12 @@ window.onload = function init() {
     if(!gl) { alert("WebGL isn't available"); }
     
     // Configure WebGL
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight - window.innerHeight*0.3;
-    gl.viewport(0,0, canvas.width, canvas.height);
+    updateCanvas();
     gl.clearColor(0.5, 0.5, 0.5, 1.0);
 
-    addEventListeners();
-
-    cubeInit(gl);
-    sphereInit(gl);
-    cylinderInit(gl);
-    torusInit(gl);
-    bunnyInit(gl);
+    initObjects();
     create('cube');
 
-    var at = [0, 0, 0];
-    var eye = [1, 1, 1];
-    var up = [0, 1, 0];
-    mView = lookAt(eye, at, up);
-    mProjection = ortho(-2, 2, -2, 2, 10, -10);
-    
     // Load shaders and initialize attribute buffers
     program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
@@ -43,69 +31,53 @@ window.onload = function init() {
     mViewLocation = gl.getUniformLocation(program, "mView");
     mProjectionLocation = gl.getUniformLocation(program, "mProjection");
 
-    //resizeCanvas();
+    addEventListeners();
+
     render();
+}
+
+function initObjects()
+{
+    cubeInit(gl);
+    sphereInit(gl);
+    cylinderInit(gl);
+    torusInit(gl);
+    bunnyInit(gl);
 }
 
 function handleWheel(e)
 {
     var e_delta = (e.deltaY || -e.wheelDelta || e.detail);
     var delta =  e_delta && ((e_delta >> 10) || 1) || 0;
-    zoom = (delta > 0 || event.detail > 0) ? 1.1 : 0.9;
-    sx *= zoom;
-    sz *= zoom;
-    //sy = canvas.width/canvas.height >= 1 ? canvas.width/canvas.height : 1;
-    sy *= zoom;
-    currentInstance.m = mult(mat4(), scalem(sx,sy, sz));
+    zoom *= (delta > 0 || event.detail > 0) ? 1.1 : 0.9;
+    updateCanvas();
+    currentInstance.m = mat4();
 }
 
 function addEventListeners()
 {
-    /*
-    document.getElementById("tx").addEventListener("input", function()
-    {tx = this.value; updateMatrixModel();});
-    document.getElementById("ty").addEventListener("input", function()
-    {ty = this.value; updateMatrixModel();});
-    document.getElementById("tz").addEventListener("input", function()
-    {tz = this.value; updateMatrixModel();});
-    document.getElementById("rx").addEventListener("input", function()
-    {rx = this.value; updateMatrixModel();});
-    document.getElementById("ry").addEventListener("input", function()
-    {ry = this.value; updateMatrixModel();});
-    document.getElementById("rz").addEventListener("input", function()
-    {rz = this.value; updateMatrixModel();});
-    document.getElementById("sx").addEventListener("input", function()
-    {sx = this.value; updateMatrixModel();});
-    document.getElementById("sy").addEventListener("input", function()
-    {sy = this.value; updateMatrixModel();});
-    document.getElementById("sz").addEventListener("input", function()
-    {sz = this.value; updateMatrixModel();});*/
     document.getElementById("cube").addEventListener("click", function () {create('cube');});
     document.getElementById("sphere").addEventListener("click", function () {create('sphere')});
     document.getElementById("cylinder").addEventListener("click", function () {create('cylinder')});
     document.getElementById("torus").addEventListener("click", function () {create('torus')});
     document.getElementById("bunny").addEventListener("click", function () {create('bunny')});
     canvas.addEventListener("wheel", function(){handleWheel(event);});
-    
-    //document.getElementById("resetCurrent").addEventListener("click", resetCurrent);
-    //document.getElementById("resetAll").addEventListener("click", resetAll);
     addEventListener("keypress", keyPress);
-    window.addEventListener('resize', resizeCanvas, false);
+    window.addEventListener('resize', updateCanvas, false);
 }
 
-function resizeCanvas()
+function updateCanvas()
 {
     canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight - window.innerHeight*0.3;
+    canvas.height = window.innerHeight*HEIGHT_RATIO;
+    var aspectRatio = canvas.width/canvas.height;
     gl.viewport(0,0, canvas.width, canvas.height);
-    var sxTemp = canvas.height/canvas.width >= 1 ? canvas.height/canvas.width : 1;
-    sx = 1;
-    sy = canvas.width/canvas.height >= 1 ? canvas.width/canvas.height : 1;
-    sz = 1;
-    sx *= zoom;
-    sy *= zoom;
-    sz *= zoom;
-    currentInstance.m = mult(mat4(), scalem(sx,sy, sz));
+
+    var at = [0, 0, 0];
+    var eye = [1, 1, 1];
+    var up = [0, 1, 0];
+    mView = lookAt(eye, at, up);
+    mProjection = ortho(-1*aspectRatio * zoom, 1*aspectRatio * zoom, -1 * zoom, 1*zoom, 10, -10);
 }
 
 function keyPress(ev)
@@ -129,7 +101,7 @@ function create(type) {
     tx = 0; ty = 0; tz = 0;
     rx = 0; ry = 0; rz = 0;
     sx = 1; sy = 1; sz = 1;
-    var m = mult(mat4(), scalem(1,canvas.width/canvas.height, 1)); //mult(translate(tx, ty, tz), mult(rotateX(rx), mult(rotateY(ry), mult(rotateZ(rz), scalem(1, canvas.width/canvas.height, 1)))));
+    var m = mat4(); //mult(translate(tx, ty, tz), mult(rotateX(rx), mult(rotateY(ry), mult(rotateZ(rz), scalem(1, canvas.width/canvas.height, 1)))));
     switch(type)
     {
         
@@ -140,40 +112,18 @@ function create(type) {
         case 'bunny': currentInstance = {m: m, f: bunnyDraw}; break;
     }
     document.getElementById(type).checked = true;
-    resizeCanvas();
-    //nInstances++;
+    updateCanvas();
 }
-
-function resetCurrent() {
-    tx = 0; ty = 0; tz = 0;
-    rx = 0; ry = 0; rz = 0;
-    sx = 1; sy = 1; sz = 1;
-
-    //updateMatrixModel();
-}
-/*
-function resetAll() {
-    //instances = [];
-    //nInstances = 0;
-    create('cube');
-    resetCurrent();
-}*/
-/*
-function updateMatrixModel() {
-    /*instances[nInstances - 1].mcurrentInstance.m = mult(translate(tx, ty, tz), mult(rotateX(rx), mult(rotateY(ry), mult(rotateZ(rz), scalem(sx, sy, sz)))));
-}*/
 
 function render() {
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     gl.uniformMatrix4fv(mViewLocation, false, flatten(mView));
     gl.uniformMatrix4fv(mProjectionLocation, false, flatten(mProjection));
-    /*
-    for (var i = 0; i < nInstances; i++) {
-        gl.uniformMatrix4fv(mModelLocation, false, flatten(instances[i].m));
-        instances[i].f(gl, program, isFilled);
-    }*/
+
     gl.uniformMatrix4fv(mModelLocation, false, flatten(currentInstance.m));
     gl.cullFace(gl.BACK);
+    
     currentInstance.f(gl, program, isFilled);
+        
     requestAnimFrame(render);
 }
